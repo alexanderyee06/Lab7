506ebfc1634da23fd1434425444068e9415b0b60
alexanderyee06 <alexander_yee06@hotmail.com>
2022-03-23 18:04:30 -0600

    Initial commit

---
+++ src/main/java/ca/sait/lab6/services/RoleService.java
@@ -0,0 +1,23 @@
+package ca.sait.lab6.services;
+
+
+import ca.sait.lab6.dataaccess.RoleDB;
+import ca.sait.lab6.dataaccess.UserDB;
+import ca.sait.lab6.models.Role;
+import ca.sait.lab6.models.User;
+import java.util.List;
+
+
+public class RoleService {
+private UserDB userDB = new UserDB();
+
+    private RoleDB roleDB = new RoleDB();
+
+    public List<Role> getAll() throws Exception {
+
+        List<Role> roles = this.roleDB.getAll();
+        return roles;
+    }
+}
+    
+   
